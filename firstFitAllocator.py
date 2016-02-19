d2b5991de153f7a7d2a77ff52f28f7808d655708
Aaron W. Tabor <aarontabor50@gmail.com>
2016-02-19 09:37:34 -0400

    Optimization. allocate() pops allocated node based on index.
    Upon successful allocation, a node must be removed from the free list. This
    removal was being performed using a linear search, which was unnecessary since
    we already knew the index of the to-be-removed node.

--- firstFitAllocator.py
+++ firstFitAllocator.py
@@ -9,11 +9,13 @@ class FirstFitAllocator:
 		self.free_list = free_list
 
 	def allocate(self, num_bytes):
-		for node in self.free_list:
+		for index, node in enumerate(self.free_list):
 			if node.num_bytes >= num_bytes:
-				self.free_list.remove(node)
+				self.free_list.pop(index)
 				if node.num_bytes > num_bytes:
 					new_node = FreeListNode(node.heap_index+num_bytes, node.num_bytes-num_bytes)
+
+					# target for future optimization, I know exactly where it goes
 					add_node(self.free_list, new_node)
 				return node.heap_index
 		raise OutOfMemoryException
